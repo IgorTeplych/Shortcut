3a8a8bbe3f17b690a6e2e901872a31575e2bed19
Igor Teplych <igorvrt@mail.ru>
2023-01-22 15:02:16 +0600

    Реализовал алгоритм Дейкстры

---
+++ Shortcut/BasicStructures/SingleArray.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicStructures
+{
+    public class SingleArray<T> : IArray<T>
+    {
+        T[] array;
+        public SingleArray()
+        {
+            array = new T[0];
+        }
+        public int Size { get { return array.Length; } }
+        public T[] Array
+        {
+            get { return array; }
+        }
+        public void Add(T item, int index)
+        {
+            int length = index + 1;
+            T[] tempArray = new T[length];
+            tempArray[index] = item;
+
+            for (long i = 0; i < array.Length; i++)
+            {
+                tempArray[i] = array[i];
+            }
+            array = tempArray;
+        }
+        public T Get(int idx)
+        {
+            return array[idx];
+        }
+        public T Remove(int index)
+        {
+            T[] tempArray = new T[array.Length - 1];
+
+            long count = 0;
+            while (count < index - 1)
+            {
+                count++;
+                tempArray[count] = array[count];
+            }
+            count++;
+            while (count++ < tempArray.Length)
+            {
+                tempArray[count - 1] = array[count];
+            }
+
+            T outVal = array[index];
+            array = tempArray;
+            return outVal;
+        }
+    }
+}
---
+++ Shortcut/Dijkstra.cs
@@ -0,0 +1,66 @@
+﻿using BasicStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shortcut
+{
+    public class Dijkstra
+    {
+        int infinity = int.MaxValue;
+        int[] price;
+        int[][] G;
+        SingleArray<int> path;
+        int startVertex;
+        public void GetShortPath(int[][] g, int startVertex, int endVertex)
+        {
+            G = g;
+            this.startVertex = startVertex;
+            path = new SingleArray<int>();
+            price = new int[g.Length];
+            for (int i = 0; i < price.Length; i++)
+                price[i] = int.MaxValue;
+
+            price[startVertex] = 0;
+
+            //Resolvening the price of all vertexes
+            for (int i = startVertex; i < g.Length; i++)
+            {
+                int[] h = g[i];
+                for (int j = 0; j < h.Length; j++)
+                {
+                    if (h[j] != infinity)
+                    {
+                        if (price[i] + h[j] < price[j])
+                        {
+                            price[j] = price[i] + h[j];
+                        }
+                    }
+                }
+            }
+            SearchPath(endVertex);
+        }
+
+        void SearchPath(int vertex)
+        {
+            path.Add(vertex, path.Size);
+
+            if (vertex == startVertex)
+                return;
+
+            int[] h = G[vertex];
+            for (int i = 0; i < h.Length; i++)
+            {
+                int prc = price[vertex];
+                if (h[i] != 0 && h[i] != infinity)
+                {
+                    if (prc - h[i] == price[i])
+                        SearchPath(i);
+                }
+            }
+        }
+
+    }
+}
